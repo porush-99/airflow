b6a514d83466c16a1b69caeec76581396bf3e7fa
Porush Goyal <40556902+porush-99@users.noreply.github.com>
2024-01-03 22:57:19 +0530

    Update test_dag_integrity.py

--- tests/test_dag_integrity.py
+++ tests/test_dag_integrity.py
@@ -1,19 +1,46 @@
+import unittest
 from unittest.mock import patch, MagicMock
 from airflow.models import DagBag
 from dags.firstdag import capture_api_response
 
 
-def test_dag_bag():
-    dag_bag = DagBag(include_examples=False)
-    assert not dag_bag.import_errors
+class TestAirflowDag(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.dag_bag = DagBag(include_examples=False)
 
-@patch('dags.firstdag.requests')
-def test_capture_api_response(mock_requests):
-    mock_response = MagicMock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = {'description': 'test response'}
+    def test_dag_bag(self):
+        # dag_bag = DagBag(include_examples=False)
+        dag = self.dag_bag.get_dag(dag_id='first_dag')
+        assert self.dag_bag.import_errors == {}
+        assert dag is not None
 
-    mock_requests.get.return_value = mock_response
+    @staticmethod
+    def AssertDagDictEqual(source, dag):
+        print('source keys')
+        print(source)
+        print(list(dag.task_dict.keys()))
+        assert list(dag.task_dict.keys()) == source
 
-    assert capture_api_response() is 'test response'
+    def test_dag(self):
+        self.AssertDagDictEqual([
+            "start",
+            "capture_response",
+            "end"
+        ], self.dag_bag.get_dag(dag_id='first_dag'))
+
+    @patch('dags.firstdag.requests')
+    def test_capture_api_response(self, mock_requests):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {'description': 'test response'}
+
+        mock_requests.get.return_value = mock_response
+
+        self.assertEqual(capture_api_response(), 'test response')
+
+        mock_response.status_code = 404
+        mock_requests.get.return_value = mock_response
+
+        self.assertEqual(capture_api_response(), 'no response')
